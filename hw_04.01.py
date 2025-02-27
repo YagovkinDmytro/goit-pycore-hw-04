b17df1d6ea549b5d7652d40feb60c0b93a6f3dd7
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2025-02-27 22:25:05 +0100

    function total_salary

---
+++ hw_04.01.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+path_to_employees_file = Path("employees.txt")
+
+def total_salary(path):
+        with open(path, "r", encoding="utf-8") as employees_file:
+            employees_info = [el.strip().split(",") for el in employees_file.readlines()]   
+
+        total = 0
+        for employee in employees_info:
+            try:
+                total += int(employee[1])
+            except (ValueError, IndexError):
+                raise ValueError(f"Error in line: {employee}")
+        
+        if len(employees_info) == 0:
+            raise ValueError("File is empty or contains invalid data")
+        
+        average = int(total / len(employees_info)) 
+        return total, average
+        
+    
+
+if path_to_employees_file.exists():
+    try:
+        total, average = total_salary(path_to_employees_file)
+        print(f"Total salary: {total}, average salary: {average}")
+    except ValueError as e:
+        print(f"Data processing error: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+else:
+    print("File not found")
