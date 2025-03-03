c30103e7671962af4fed6126d05fc8b46f263e48
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2025-03-03 17:54:41 +0100

    function list_of_folder

--- hw_04_03.py
+++ hw_04_03.py
@@ -1,30 +1,28 @@
 import sys
 from pathlib import Path
-from colorama import init, Fore, Style
+from colorama import init, Fore
 
-def list_of_folder(path):
+init(autoreset=True)
+
+def list_of_folder(path, indent="  "):
         for el in path.iterdir():
             if el.is_dir():
-                list_of_folder(el)
-                print(el.name)
+                print(f"{indent}{Fore.BLUE}📁 {el.name}")
+                list_of_folder(el, indent + "  ")
             else:
-                print(el.name)
-
+                print(f"{indent}{Fore.GREEN}📄 {el.name}")
+                
 def main():
-    if len(sys.argv) >= 1:
+    if len(sys.argv) > 1:
         path_to_directory = Path(sys.argv[1])
         if path_to_directory.exists() and path_to_directory.is_dir():
             directory = Path(path_to_directory)
+            print(f"{Fore.YELLOW}📁 {path_to_directory.name}")
             list_of_folder(directory)
-            print(f"Name of the directory: {path_to_directory.name}")
-            print(f"Name: {directory}")
         else:
-            print(f"Invalid directory path: {directory}")
-
-        # print(f"Name of the directory: {directory.name}")
-        # print(f"Path to the folder: {sys.argv[1]}")
+            print(f"{Fore.RED}The directory path is invalid or does not exist: {path_to_directory}")
     else:
-         print(f"Arguments are not provided")
+         print(f"{Fore.YELLOW}Arguments are not provided")
 
 if __name__ == "__main__":
     main()
